9ad0095267680d36b5a3326859afdeb66ecac3bc
Lars Henrik Bolstad <lahebo@gmail.com>
2015-10-27 13:47:39 +0100

    Added footer component.

---
+++ app/components/et-footer/et-footer.ctrl.js
@@ -0,0 +1,18 @@
+(function(){
+	'use strict';
+
+	/**
+	 * @ngdoc function
+	 * @name etApp.controller:EtFooterCtrl
+	 * @description
+	 * # EtFooterCtrl
+	 * Controller of the etApp
+	 */
+	angular.module('etApp')
+		.controller('etFooterCtrl', etFooterCtrl);
+
+		/*@ngInject*/
+		function etFooterCtrl() {
+
+		};
+})();
