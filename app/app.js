84bd6ccaf43111b4f0a9e50ae7a6421ea586f2b4
Glenn Flø Karlsen <glenn.fk@hotmail.com>
2015-11-12 08:43:40 +0100

    Fixed navigation

--- app/app.js
+++ app/app.js
@@ -43,21 +43,21 @@
  	      templateUrl: 'partials/companies/companies.html',
  	      controller: 'CompaniesCtrl as pagecomp'
  	    })
- 	    .state('board', {
- 	      url: '/board',
- 	      templateUrl: 'partials/board/board.html',
- 	      controller: 'BoardCtrl'
- 	    })
- 	    .state('login', {
- 	      url: '/login',
- 	      templateUrl: 'partials/login/login.html',
- 	      controller: 'LoginCtrl as LOGIN'
- 	    })
- 	    .state('register', {
- 	      url: '/register/:user',
- 	      templateUrl: 'partials/register/register.html',
- 	      controller: 'RegisterCtrl'
- 	    })
+ 	    // .state('board', {
+ 	    //   url: '/board',
+ 	    //   templateUrl: 'partials/board/board.html',
+ 	    //   controller: 'BoardCtrl'
+ 	    // })
+ 	    // .state('login', {
+ 	    //   url: '/login',
+ 	    //   templateUrl: 'partials/login/login.html',
+ 	    //   controller: 'LoginCtrl as LOGIN'
+ 	    // })
+ 	    // .state('register', {
+ 	    //   url: '/register/:user',
+ 	    //   templateUrl: 'partials/register/register.html',
+ 	    //   controller: 'RegisterCtrl'
+ 	    // })
  	    .state('contact',{
  	    	url: '/contact',
  	    	templateUrl: 'partials/contact/contact.html',
