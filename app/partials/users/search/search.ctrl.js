3dfde47d3d2e7b7ab396093ce0981b81c491bdcf
Lars Henrik Bolstad <lahebo@gmail.com>
2015-12-02 14:24:23 +0100

    Added filtering (draft) to search page. Modified profile page.

--- app/partials/users/search/search.ctrl.js
+++ app/partials/users/search/search.ctrl.js
@@ -12,12 +12,17 @@
 	  .controller('SearchCtrl', SearchCtrl);
 
 		/*@ngInject*/
-	  	function SearchCtrl($scope) {
-		    $scope.awesomeThings = [
-		      'HTML5 Boilerplate',
-		      'AngularJS',
-		      'Karma'
-		    ];
+	  	function SearchCtrl() {
+		    this.users = [
+					{img: '/assets/images/icons/avatar.jpg', name: 'Winston Northman', usertype: 'student'},
+					{img: '/assets/images/icons/avatar.jpg', name: 'Winston Eastman', usertype: 'student'},
+					{img: '/assets/images/icons/avatar.jpg', name: 'Winston Westman', usertype: 'student'},
+					{img: '/assets/images/icons/avatar.jpg', name: 'Winston Southman', usertype: 'student'},
+					{img: '/assets/images/icons/avatar.jpg', name: 'hr Winston Southbeach', usertype: 'student'},
+					{img: '/assets/images/icons/avatar.jpg', name: 'Northman Inc.', usertype: 'bedrift'},
+					{img: '/assets/images/icons/avatar.jpg', name: 'Northman AS', usertype: 'bedrift'},
+					{img: '/assets/images/icons/avatar.jpg', name: 'Winston Company', usertype: 'bedrift'}
+				]
 		}
-		SearchCtrl.$inject = ['$scope'];
+		SearchCtrl.$inject = [];
 })();
